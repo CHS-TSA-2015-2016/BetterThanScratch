8660dfe557fbdc3045143e6426ec57bf3db2ba23
ultracodingmaster <broughspencer@gmail.com>
2015-11-11 10:17:52 -0700

    Update ScriptingWindow.py

--- src/ScriptingWindow.py
+++ src/ScriptingWindow.py
@@ -1,6 +1,7 @@
 __author__ = 'Spencer'
 
-from Tkinter import *;
+import Tkineter as tk;
+from tk import *;
 from ScrolledText import ScrolledText;
 """
     this is a not permanent scripting window so that for now, we can type our lwsl and run it, contained in a ui
@@ -12,13 +13,13 @@ def run(event):
     code = scriptInput.get("1.0", "end-1c");
     print(code);
 
-root = Tk();
+root = tk.Tk();
 
-rootFrame = Frame(root, width=800, height=600);
+rootFrame = tk.Frame(root, width=800, height=600);
 rootFrame.pack();
 
-scriptInput = ScrolledText(rootFrame);
-scriptButton = Button(rootFrame, text="Run");
+scriptInput = tk.ScrolledText(rootFrame);
+scriptButton = tk.Button(rootFrame, text="Run");
 scriptButton.bind("<ButtonPress>", run);
 
 scriptInput.grid(columnspan=2);
